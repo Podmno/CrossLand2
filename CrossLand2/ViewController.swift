53732e53fc7255ee0263b0c8ae76033ba409f1e1
Podmno <podmno@icloud.com>
2024-02-07 20:43:02 +0800

    Initial Commit

---
+++ CrossLand2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrossLand2
+//
+//  Created by Ki MNO on 2024/2/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
